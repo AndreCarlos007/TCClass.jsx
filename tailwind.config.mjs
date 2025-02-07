69bcfac5fe3ce0616f8e0e68bc3bd4a091c36e88
AndreCarlos007 <andreoliveiracx007@gmail.com>
2025-02-06 21:57:05 -0300

    :sparkles: feat: Commit inicial

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,23 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'sm': '360px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        colorOne: "var(--color-one)",
+        colorTwo: "var(--color-two)",
+        colorTree: "var(--color-tree)",
+        colorFor: "var(--color-for)",
+
+        colorPrimary: "var(--color-primary)",
+        colorSecundary: "var(--color-secundary)",
+        colorWhite: "var(--color-textWhite)"
       },
     },
   },
